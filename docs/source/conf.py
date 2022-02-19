3340554b1e4a26f09c398892c920ec945a2c7f15
Vittorio Caggiano <caggiano@gmail.com>
2022-02-19 12:17:44 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
-sys.path.insert(0, os.path.abspath("../../myoSuite"))
+sys.path.insert(0, os.path.abspath("../../myosuite"))
 
 # -- Project information
 
