25160071a6ce756944494fa2c258d440a5da6131
Vittorio Caggiano <caggiano@fb.com>
2022-11-12 10:39:26 -0500

    1.2.4 release

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ mjc_models_files = package_files('myosuite/envs/myo/assets/','.mjb')
 if __name__ == "__main__":
     setup(
         name="MyoSuite",
-        version="1.2.3",
+        version="1.2.4",
         author='MyoSuite Authors - Vikash Kumar (Meta AI), Vittorio Caggiano (Meta AI), Huawei Wang (University of Twente), Guillaume Durandau (University of Twente), Massimo Sartori (University of Twente)',
         author_email="vikashplus@gmail.com",
         license='Apache 2.0',
