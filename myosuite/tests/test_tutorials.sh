9234ce5fd53f3767e7e7793767c4c4d4b0f6def9
Vittorio Caggiano <caggiano@fb.com>
2023-04-09 16:13:36 -0400

    remove tutorial on training

--- myosuite/tests/test_tutorials.sh
+++ myosuite/tests/test_tutorials.sh
@@ -18,7 +18,7 @@ python -m ipykernel install --user --name=$CONDA_DEFAULT_ENV
 declare -a StringArray=("1_Get_Started.ipynb" \
                          "2_Load_policy.ipynb" \
                          "3_Analyse_movements.ipynb" \
-                         "4_Train_policy.ipynb" \
+                        #  "4_Train_policy.ipynb" \
                          "5_Move_Hand_Fingers.ipynb" )
 
 # Iterate the string array using for loop
