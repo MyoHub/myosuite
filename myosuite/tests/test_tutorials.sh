0f9f3cb60cb16e3d5d18f05e80f1be2fa027c0b7
Vittorio Caggiano <caggiano@gmail.com>
2023-12-17 11:37:45 -0500

    add deprl package

--- myosuite/tests/test_tutorials.sh
+++ myosuite/tests/test_tutorials.sh
@@ -14,7 +14,7 @@ pip3 install scikit-learn
 pip3 install jupyter ipykernel tabulate matplotlib torch h5py
 pip3 install git+https://github.com/aravindr93/mjrl.git@pvr_beta_1vk # install from branch `pvr_beta_1vk` compatible with `mujoco` native binding
 pip3 install stable-baselines3
-# pip3 install deprl
+pip3 install deprl
 python3 -m ipykernel install --user --name=$CONDA_DEFAULT_ENV
 
 # Tested tutorials
