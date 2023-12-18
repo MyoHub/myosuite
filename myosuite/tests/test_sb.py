a45eb971f1c4a094dd2cdbdedc8ba5c072ff3f89
Vittorio Caggiano <caggiano@gmail.com>
2023-12-18 15:39:45 -0500

    add test sb3 with gym/gymnasium only on a smaller subset of tasks

--- myosuite/tests/test_sb.py
+++ myosuite/tests/test_sb.py
@@ -4,11 +4,11 @@ from myosuite.utils import gym
 
 from myosuite import myosuite_myobase_suite
 from myosuite import myosuite_myochal_suite
-for env_name in myosuite_myobase_suite+myosuite_myochal_suite:
+for env_name in myosuite_myochal_suite:#myosuite_myobase_suite+myosuite_myochal_suite:
     print("\n\n"+env_name + " ---- Starting policy learning")
     env = gym.make(env_name)
     model = PPO("MlpPolicy", env, verbose=0)
     print("========================================")
-    model.learn(total_timesteps=10)
+    model.learn(total_timesteps=2)
 
 
