6d4c4780bd55c1c79007e7651421199cce39df58
Vittorio Caggiano <caggiano@gmail.com>
2023-12-18 15:39:00 -0500

    add test for sb for both gym and gymnasium

--- myosuite/tests/test_versions.sh
+++ myosuite/tests/test_versions.sh
@@ -9,6 +9,7 @@ pip uninstall -y gym
 echo "=================== Testing gym==0.26.2 ==================="
 pip install gym==0.26.2
 python myosuite/tests/test_myo.py
+python myosuite/tests/test_sb.py
 pip uninstall -y gym
 
 echo "=================== Testing gymnasium ==================="
