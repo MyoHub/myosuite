e38958327d23d9c8221d5318f4d6355344852ad2
Vittorio Caggiano <caggiano@gmail.com>
2023-12-28 23:08:50 +0100

    add muscle specific smooth coeefficients

--- myosuite/envs/myo/fatigue.py
+++ myosuite/envs/myo/fatigue.py
@@ -2,25 +2,33 @@ import numpy as np
 
 class CumulativeFatigue():
     # 3CC-r model, adapted from https://dl.acm.org/doi/pdf/10.1145/3313831.3376701 for muscles 
-    # based on the implementation form Aleksi Ikkala and Florian Fischer https://github.com/aikkala/user-in-the-box/blob/main/uitb/bm_models/effort_models.py
+    # based on the implementation from Aleksi Ikkala and Florian Fischer https://github.com/aikkala/user-in-the-box/blob/main/uitb/bm_models/effort_models.py
     def __init__(self, mj_model):
-        # self._r = 15 # Recovery time multiplier i.e. how many times more than during rest intervals https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6092960/
-        # self._F = 0.0146 # Fatigue coefficients
-        # self._R = 0.0022 # Recovery coefficients
-        self._r = 25 # Recovery time multiplier i.e. how many times more than during rest intervals
-        self._F = 0.5 # Fatigue coefficients
-        self._R = 0.01 # Recovery coefficients
-        self._LD = 1/0.01
-        self._LR = 1/0.04
+        self._r = 15 # Recovery time multiplier i.e. how many times more than during rest intervals https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6092960/
+        self._F = 0.0146 # Fatigue coefficients
+        self._R = 0.0022 # Recovery coefficients
         self.na = mj_model.na
         self._dt = mj_model.opt.timestep
+        self._LD = np.array([1/mj_model.actuator_dynprm[i][0] for i in range(self.na)])
+        self._LR = np.array([1/mj_model.actuator_dynprm[i][1] for i in range(self.na)])
         self._MA = np.zeros((self.na,))  # Muscle Active
         self._MR = np.ones((self.na,))   # Muscle Resting
         self._MF = np.zeros((self.na,))  # Muscle Fatigue
         self.TL  = np.zeros((self.na,))  # Target Load
 
+    def set_FatigueCoefficient(self, F):
+        # Set Fatigue coefficients
+        self._F = F
+    
+    def set_RecoveryCoefficient(self, R):
+        # Set Recovery coefficients
+        self._R = R
+    
+    def set_RecoveryMultiplier(self, r):
+        # Recovery time multiplier
+        self._r = r
+        
     def compute_act(self, act):
-
         # Get target load (actual activation, which might be reached only with some "effort", 
         # depending on how many muscles can be activated (fast enough) and how many are in fatigue state)
         self.TL = act 
@@ -28,11 +36,11 @@ class CumulativeFatigue():
         # Calculate C(t) -- transfer rate between MR and MA
         C = np.zeros_like(self._MA)
         idxs = (self._MA < self.TL) & (self._MR > (self.TL - self._MA))
-        C[idxs] = self._LD * (self.TL[idxs] - self._MA[idxs])
+        C[idxs] = self._LD[idxs] * (self.TL[idxs] - self._MA[idxs])
         idxs = (self._MA < self.TL) & (self._MR <= (self.TL - self._MA))
-        C[idxs] = self._LD * self._MR[idxs]
+        C[idxs] = self._LD[idxs] * self._MR[idxs]
         idxs = self._MA >= self.TL
-        C[idxs] = self._LR * (self.TL[idxs] - self._MA[idxs])
+        C[idxs] = self._LR[idxs] * (self.TL[idxs] - self._MA[idxs])
 
         # Calculate rR
         rR = np.zeros_like(self._MA)
@@ -54,11 +62,11 @@ class CumulativeFatigue():
         return self._MA, self._MR, self._MF
 
     def get_effort(self):
-            # Calculate effort
-            return np.linalg.norm(self._MA - self.TL)
+        # Calculate effort
+        return np.linalg.norm(self._MA - self.TL)
 
     def reset(self):
-            self._MA = np.zeros((self.na,)) # Muscle Active
-            self._MR = np.ones((self.na,))  # Muscle Resting
-            self._MF = np.zeros((self.na,)) # Muscle Fatigue
+        self._MA = np.zeros((self.na,))  # Muscle Active
+        self._MR = np.ones((self.na,))   # Muscle Resting
+        self._MF = np.zeros((self.na,))  # Muscle Fatigue
 
