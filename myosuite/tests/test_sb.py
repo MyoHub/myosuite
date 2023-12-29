d02006c347d348ed0df9e5f65023be3aaa862f25
Vikash Kumar <vikashplus@gmail.com>
2023-12-29 18:59:33 -0500

    FEATURE: Moving gym import to __init__ for a shorter import statement

--- myosuite/tests/test_sb.py
+++ myosuite/tests/test_sb.py
@@ -1,6 +1,6 @@
 from stable_baselines3 import PPO
 import myosuite
-from myosuite.utils.import_utils import gym
+from myosuite.utils import gym
 
 from myosuite import myosuite_myobase_suite
 from myosuite import myosuite_myochal_suite
