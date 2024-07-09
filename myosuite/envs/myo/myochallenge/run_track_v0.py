changes to environmetn to fix reward computation and to make heightfields work

myosuite/envs/myo/myochallenge/run_track_v0.py
@@ -1,9 +1,6 @@
-""" =================================================
-# Copyright (c) Facebook, Inc. and its affiliates
-Authors  :: Vikash Kumar (vikashplus@gmail.com), Vittorio Caggiano (caggiano@gmail.com), Pierre Schumacher (schumacherpier@gmail.com), Chun Kwang Tan (cktan.neumove@gmail.com)
-================================================= """
-
-import collections
+""" ================================================= # Copyright (c) Facebook, Inc. and its affiliates Authors  :: Vikash Kumar (vikashplus@gmail.com), Vittorio Caggiano (caggiano@gmail.com), Pierre Schumacher (schumacherpier@gmail.com), Chun Kwang Tan (cktan.neumove@gmail.com) ================================================= 
+""" 
+import collections 
 from myosuite.utils import gym
 import numpy as np
 import pink
@@ -45,7 +42,7 @@ class RunTrack(WalkEnvV0):
     # You can change reward weights here
     DEFAULT_RWD_KEYS_AND_WEIGHTS = {
         "sparse": 1,
-        "solved": -10,
+        "solved": +10,
     }
 
     def __init__(self, model_path, obsd_model_path=None, seed=None, **kwargs):
@@ -77,9 +74,9 @@ class RunTrack(WalkEnvV0):
                hills_difficulties=(0,0),
                rough_difficulties=(0,0),
                stairs_difficulties=(0,0),
-               real_length=20,
+               real_length=15,
                real_width=1,
-               distance_thr = 20,
+               distance_thr = 10,
                **kwargs,
                ):
 
@@ -151,7 +148,7 @@ class RunTrack(WalkEnvV0):
 
         # The task is entirely defined by these 3 lines
         score = self.get_score()
-        win_cdt = 0
+        win_cdt = self._win_condition()
         rwd_dict = collections.OrderedDict((
             # Perform reward tuning here --
             # Update Optional Keys section below
@@ -214,7 +211,8 @@ class RunTrack(WalkEnvV0):
         if not self.trackfield is None:
             self.trackfield.sample(self.np_random)
             self.sim.model.geom_rgba[self.sim.model.geom_name2id('terrain')][-1] = 1.0
-            self.sim.model.geom_pos[self.sim.model.geom_name2id('terrain')] = np.array([0, -10, 0.005])
+            # self.sim.model.geom_pos[self.sim.model.geom_name2id('terrain')] = np.array([0, -10, 0.005])
+            self.sim.model.geom_pos[self.sim.model.geom_name2id('terrain')] = np.array([0, 0, 0.005])
         else:
             # move trackfield down if not used
             self.sim.model.geom_rgba[self.sim.model.geom_name2id('terrain')][-1] = 0.0
@@ -287,7 +285,7 @@ class RunTrack(WalkEnvV0):
         
         qpos[3:7] = rot_state
         qpos[2] = height
-        qpos[1] = 4.5
+        qpos[1] = 15
         return qpos, qvel
 
     def _setup_convenience_vars(self):
@@ -311,7 +309,7 @@ class RunTrack(WalkEnvV0):
 
     def _win_condition(self):
         y_pos = self.obs_dict['model_root_pos'].squeeze()[1]
-        if y_pos > self.distance_thr:
+        if np.abs(y_pos) > self.distance_thr:
             return 1
         return 0
 
@@ -320,7 +318,7 @@ class RunTrack(WalkEnvV0):
         y_pos = self.obs_dict['model_root_pos'].squeeze()[1]
         if x_pos > self.real_width or x_pos < - self.real_width:
             return 1
-        if y_pos > 5.0:
+        if y_pos > 15.0:
             return 1
         return 0
 
@@ -338,7 +336,7 @@ class RunTrack(WalkEnvV0):
         if not self.startFlag:
             return -1
         y_pos = self.obs_dict['model_root_pos'].squeeze()[1] if self.startFlag else -1
-        score = y_pos / (self.horizon * self.distance_thr)
+        score = (y_pos - 15) / (- 15 - 15)
         return score.squeeze()
 
     def _get_muscle_lengthRange(self):