71e709ef1c3ed10bbb6cefc57e6af7420080be2c
Cheryl Wang <90352960+cherylwang20@users.noreply.github.com>
2024-10-23 12:51:59 -0400

    BUGFIX: Goal Condition

--- myosuite/envs/myo/myochallenge/bimanual_v0.py
+++ myosuite/envs/myo/myochallenge/bimanual_v0.py
@@ -50,7 +50,7 @@ class BimanualEnvV1(BaseV0):
                goal_center=np.array([0.4, -0.25, 1.05]),
                max_force=1500,  # Max force against throwing
 
-               proximity_th=0.015,  # object-target proximity threshold
+               proximity_th=0.15,  # object-target proximity threshold
 
                start_shifts=np.array([0.055, 0.055, 0]),
                # shift factor for start/goal random generation with z-axis fixed
@@ -246,6 +246,7 @@ class BimanualEnvV1(BaseV0):
         obj_pos = obs_dict["obj_pos"][0][0] if obs_dict['obj_pos'].ndim == 3 else obs_dict['obj_pos']
         palm_pos = obs_dict["palm_pos"][0][0] if obs_dict["palm_pos"].ndim == 3 else obs_dict["palm_pos"]
         goal_pos = obs_dict["goal_pos"][0][0] if obs_dict["goal_pos"].ndim == 3 else obs_dict["goal_pos"]
+        goal_pos = np.concatenate((goal_pos[:2], np.array([1.09])))
 
         lift_height = np.linalg.norm(np.array([[[obj_pos[-1], palm_pos[-1]]]]) -
                                      np.array([[[self.init_obj_z, self.init_palm_z]]]), axis=-1)
