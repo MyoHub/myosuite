14f6bbe389750e12e134ff8a9b67bf9a7200e95b
Cheryl Wang <90352960+cherylwang20@users.noreply.github.com>
2025-02-28 19:10:01 -0500

    adding the base env for torso model

--- myosuite/envs/myo/myobase/__init__.py
+++ myosuite/envs/myo/myobase/__init__.py
@@ -511,23 +511,6 @@ register_env_with_variants(id='myoTorsoExoPoseFixed-v0',
     )
 
 
-register_env_with_variants(id='myoTorsoExoPoseFixed-v0',
-        entry_point='myosuite.envs.myo.myobase.torso_v0:TorsoEnvV0',
-        max_episode_steps=200,
-        kwargs={
-            'model_path': curr_dir+'/../../../simhive/myo_sim/torso/myotorso_exosuit.xml',
-            'target_jnt_range': {'flex_extension':(0, 0),'lat_bending':(-0.1, 0.1),'axial_rotation':(0, 0),
-                                 'Abs_t1':(0, 0),'Abs_t2':(0,0),'Abs_r3':(0, 0),
-                                 'L4_L5_FE':(-1, 1),'L4_L5_LB':(0, 0),'L4_L5_AR':(0, 0),
-                                 'L3_L4_FE':(-1, 1),'L3_L4_LB':(0, 0),'L3_L4_AR':(0, 0),
-                                 'L2_L3_FE':(-1, 1),'L2_L3_LB':(0, 0),'L2_L3_AR':(0, 0),
-                                 'L1_L2_FE':(-1, 1),'L1_L2_LB':(0, 0),'L1_L2_AR':(0, 0),},
-            'normalize_act': True,
-            'frame_skip': 5,
-        }
-    )
-
-
 # SAR REORIENT: 8-object ==============================
 register_env_with_variants(id='myoHandReorient8-v0',
             entry_point='myosuite.envs.myo.myobase.reorient_sar_v0:Geometries8EnvV0',
