2b0721e71d844f26d4169954f9aaf4a17cd659e7
Balint-H <bkh16@ic.ac.uk>
2025-05-29 16:56:05 +0100

    Add hand example environment also using the pose base

---
+++ myosuite/envs/myo/mjx/playground_hand_v0.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+from typing import Any, Dict, Optional, Union
+
+from etils import epath
+import jax
+import jax.numpy as jp
+from ml_collections import config_dict
+import mujoco
+from mujoco import mjx
+from mujoco_playground import State
+
+from mujoco_playground._src import mjx_env  # Several helper functions are only visible under _src
+
+from myosuite.envs.myo.mjx.playground_pose_v0 import MjxPoseEnvV0
+
+
+def default_config() -> config_dict.ConfigDict:
+    env_config = config_dict.create(
+        ctrl_dt=0.02,
+        sim_dt=0.002,
+        episode_length=100,
+        action_repeat=1,
+        action_scale=0.5,
+        history_len=1,
+        healthy_angle_range=(0, 2.1),
+        reward_config=config_dict.create(
+            angle_reward_weight=1,
+            ctrl_cost_weight=1,
+            pose_thd=0.35,
+            bonus_weight=4
+        )
+    )
+
+    rl_config = config_dict.create(
+        num_timesteps=40_000_000,
+        num_evals=16,
+        reward_scaling=0.1,
+        episode_length=env_config.episode_length,
+        clipping_epsilon=0.3,
+        normalize_observations=True,
+        action_repeat=1,
+        unroll_length=10,
+        num_minibatches=32,
+        num_updates_per_batch=8,
+        num_resets_per_eval=1,
+        discounting=0.97,
+        learning_rate=3e-4,
+        entropy_cost=0.001,
+        num_envs=8192,
+        batch_size=512,
+        max_grad_norm=1.0,
+        network_factory=config_dict.create(
+            policy_hidden_layer_sizes=(50, 50, 50),
+            value_hidden_layer_sizes=(50, 50, 50),
+            policy_obs_key="state",
+            value_obs_key="state",
+        )
+    )
+    env_config["ppo_config"] = rl_config
+    return env_config
+
+
+# TODO: Consider just registering environment variant if obs/action space is the same.
+#       For example, here overriding the preprocess function would be possible if the base class accepted
+#       a callable argument.
+class MjxHand(MjxPoseEnvV0):
+    def __init__(
+            self,
+            config: config_dict.ConfigDict = default_config(),
+            config_overrides: Optional[Dict[str, Union[str, int, list[Any]]]] = None,
+    ) -> None:
+        model_path='envs/myo/assets/hand/'
+        model_filename='myohand_pose.xml'
+        path = epath.Path(epath.resource_path('myosuite')) / model_path
+        super().__init__(path/model_filename, config, config_overrides)
+
+    def preprocess_spec(self, spec:mujoco.MjSpec):
+        spec = super().preprocess_spec(spec)
+        for s in spec.sites:
+            if "_target" in s.name:
+                print(f"Deleted target site \"{s.name}\"")
+                s.delete()
+        for t in spec.tendons:
+            if "_err" in t.name:
+                print(f"Deleted error tendon \"{t.name}\"")
+                t.delete()
+        # TODO: Verify visual geoms impact performance
+        for g in spec.geoms:
+            if not g.name or "floor" in g.name:
+                print(f"Deleted visual geom")
+                g.delete()
+
+        return spec
