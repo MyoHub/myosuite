aedd1101a03618999ec2885a526a300a87c20494
HUIYI WANG <huiyi.wang@mail.mcgill.ca>
2025-06-17 11:37:26 -0400

    new env

--- myosuite/envs/myo/myochallenge/pingpong_v0.py
+++ myosuite/envs/myo/myochallenge/pingpong_v0.py
@@ -7,6 +7,7 @@ Authors  :: Cheryl Wang (cheryl.wang.huiyi@gmail.com), Balint Hodossy (bkh16@ic.
 import collections
 import numpy as np
 from myosuite.utils import gym
+import mujoco
 
 from myosuite.envs.myo.base_v0 import BaseV0
 from myosuite.utils.quat_math import mat2euler, euler2quat
@@ -19,6 +20,7 @@ class PingPongEnvV0(BaseV0):
     DEFAULT_RWD_KEYS_AND_WEIGHTS = {
         "reach_dist": -1,
         "act": 1,
+        "sparse": 1,
         "solved": 1,
         'done': -10
     }
@@ -36,9 +38,11 @@ class PingPongEnvV0(BaseV0):
             weighted_reward_keys:list = DEFAULT_RWD_KEYS_AND_WEIGHTS,
             **kwargs,
         ):
-        self.paddle_sid = self.sim.model.site_name2id("S_grasp")
+        self.paddle_sid = self.sim.model.site_name2id("paddle")
         self.object_sid = self.sim.model.site_name2id("pingpong")
         self.object_bid = self.sim.model.body_name2id("pingpong")
+
+        self.id_info = IdInfo(self.sim.model)
 
         super()._setup(obs_keys=obs_keys,
                     weighted_reward_keys=weighted_reward_keys,
@@ -52,16 +56,14 @@ class PingPongEnvV0(BaseV0):
         obs_dict = {}
         obs_dict['time'] = np.array([sim.data.time])
 
-        obs_dict['body_qpos'] = sim.data.qpos[:-7].copy()
-        obs_dict['body_qvel'] = sim.data.qvel[:-6].copy()*self.dt
+        obs_dict['body_qpos'] = sim.data.qpos[self.id_info.myo_joint_range].copy()
+        obs_dict['body_qvel'] = sim.data.qvel[self.id_info.myo_dof_range].copy()
 
         obs_dict["ball_pos"] = sim.data.site_xpos[self.object_sid]
-        obs_dict["ball_vel"] = 
+        obs_dict["ball_vel"] = self.get_sensor_by_name(sim.model, sim.data, "pingpong_vel_sensor")
 
         obs_dict["paddle_pos"] = sim.data.site_xpos[self.paddle_sid]
-        obs_dict["paddle_vel"] = 
-
-        #obs_dict['goal_pos'] = sim.data.site_xpos[self.goal_sid] How should we specify the goal of the table? 
+        obs_dict["paddle_vel"] = self.get_sensor_by_name(sim.model, sim.data, "paddle_vel_sensor")
 
         obs_dict['reach_err'] = obs_dict['paddle_pos'] - obs_dict['ball_pos']
 
@@ -74,7 +76,6 @@ class PingPongEnvV0(BaseV0):
         reach_dist = np.abs(np.linalg.norm(self.obs_dict['reach_err'], axis=-1))
         act_mag = np.linalg.norm(self.obs_dict['act'], axis=-1)/self.sim.model.na if self.sim.model.na !=0 else 0
         ball_pos = obs_dict["ball_pos"][0][0] if obs_dict['ball_pos'].ndim == 3 else obs_dict['ball_pos']
-
         rwd_dict = collections.OrderedDict((
             # Perform reward tuning here --
             # Update Optional Keys section below
@@ -83,9 +84,10 @@ class PingPongEnvV0(BaseV0):
             # Optional Keys
             ('reach_dist', -1.*reach_dist),
             # Must keys
-            ('act_reg', -1.*act_mag),
-            ('solved', 0), ### FILL IN THE TOUCH CONDITION FOR THE OTHER END OF TABLE
-            ('done', self._get_done(ball_pos[-1])),
+            ('act', -1.*act_mag),
+            ('sparse', np.array([[ball_pos[0] < 0]])), #for reaching the other side of the table.
+            ('solved', np.array([[0]])), ### FILL IN THE TOUCH CONDITION FOR THE OTHER END OF TABLE
+            ('done', np.array([[self._get_done(ball_pos[-1])]])),
         ))
         rwd_dict['dense'] = np.sum([wt*rwd_dict[key] for key, wt in self.rwd_keys_wt.items()], axis=0)
 
@@ -124,6 +126,12 @@ class PingPongEnvV0(BaseV0):
             'effort':effort,
             }
         return metrics
+    
+    def get_sensor_by_name(self, model, data, name):
+        sensor_id = model.sensor_name2id(name)
+        start = model.sensor_adr[sensor_id]
+        dim = model.sensor_dim[sensor_id]
+        return data.sensordata[start:start+dim]
 
 
     def reset(self, reset_qpos=None, reset_qvel=None, **kwargs):
@@ -146,3 +154,20 @@ class PingPongEnvV0(BaseV0):
             self.reset(reset_qpos=reset_qpos, reset_qvel=reset_qvel,**kwargs)
 
         return obs
+    
+class IdInfo:
+    def __init__(self, model: mujoco.MjModel):
+        self.ball_body_id = model.body("pingpong").id
+
+        myo_bodies = [model.body(i).id for i in range(model.nbody)
+                    if not model.body(i).name.startswith("ping")
+                    and not model.body(i).name in ["pingpong"]]
+        self.myo_body_range = (min(myo_bodies), max(myo_bodies))
+
+        self.myo_joint_range = np.concatenate([model.joint(i).qposadr for i in range(model.njnt)
+                                            if not model.joint(i).name.startswith("ping")
+                                            and not model.joint(i).name == "pingpong_freejoint"])
+
+        self.myo_dof_range = np.concatenate([model.joint(i).dofadr for i in range(model.njnt)
+                                            if not model.joint(i).name.startswith("ping")
+                                            and not model.joint(i).name == "pingpong_freejoint"])
